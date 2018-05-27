c12d0cfb30806b01530aae846974c8979a071989
RemarkableGuy <alvaro.schipper@hotmail.com>
2018-05-28 01:25:56 +0200

    Added random tomato providers

--- src/main/java/com/relay42/tomatoes/services/TomatoSaleService.java
+++ src/main/java/com/relay42/tomatoes/services/TomatoSaleService.java
@@ -16,25 +16,23 @@ public class TomatoSaleService {
     private static final int MIN = 1;
     private static final String BEGIN = "2018-01-01 00:00:00";
 
+    private String[] providers = {"Heinz", "Hunt's", "Del Monte", "Le Ol' Granma"};
+
     public ArrayList<TomatoSale> getSales(int size) {
-        Random rand = new Random();
+        Random random = new Random();
         ArrayList<TomatoSale> list = new ArrayList<>();
-
         for (int i = 0; i < size; i++) {
-            list.add(new TomatoSale(UUID.randomUUID(), rand.nextInt(MAX - MIN + 1) + MIN, "Alvaro", this.getRandomTimestamp()));
+            list.add(new TomatoSale(UUID.randomUUID(), random.nextInt(MAX - MIN + 1) + MIN, this.providers[random.nextInt(this.providers.length)], this.getRandomTimestamp()));
         }
-
         list.sort(Collections.reverseOrder());
         return list;
     }
 
     private long getRandomTimestamp() {
-        Random rand = new Random();
-
+        Random random = new Random();
         long begin = Timestamp.valueOf(BEGIN).getTime();
         long end = System.currentTimeMillis();
         long diff = end - begin + 1;
-
-        return (long) (begin + rand.nextDouble() * diff);
+        return (long) (begin + random.nextDouble() * diff);
     }
 }
