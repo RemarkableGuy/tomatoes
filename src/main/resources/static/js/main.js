ceab1f53952a322eacc1a0bbe845690b705a9e30
RemarkableGuy <alvaro.schipper@hotmail.com>
2018-05-29 13:10:43 +0200

    Added first basic front end

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,23 @@
+window.onload = getdata(3);
+
+function getdata(size){
+    var xmlhttp = new XMLHttpRequest();
+    var url = "/data?size=" + size;
+
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            var myArr = JSON.parse(this.responseText);
+            show(myArr);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function show(arr) {
+    console.log(arr);
+    arr.forEach(function (element) {
+        var date = ((new Date(element.timestamp)));
+       document.getElementById("tbody").innerHTML += "<tr><td>" + element.provider + "</td><td>" + element.tomatoes + "</td><td>"+ date.toLocaleDateString("nl-NL") + "</td></tr>";
+    });
+}
