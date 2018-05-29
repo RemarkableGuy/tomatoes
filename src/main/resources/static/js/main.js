d69b7941b016faf4ca9d6f254db37b0eaa47e486
RemarkableGuy <alvaro.schipper@hotmail.com>
2018-05-29 23:43:39 +0200

    Removed useless brackets

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,7 +3,7 @@ window.onload = getData(3);
 function showData(data) {
     document.getElementById("tbody").innerHTML = "";
     data.forEach(function (element) {
-        var date = ((new Date(element.timestamp)));
+        var date = new Date(element.timestamp);
         document.getElementById("tbody").innerHTML += "<tr><td>" + element.provider + "</td><td>" + element.tomatoes + "</td><td>"+ date.toLocaleDateString("nl-NL") + "</td></tr>";
     });
 }
@@ -12,7 +12,7 @@ function getData(size) {
     var xmlhttp = new XMLHttpRequest();
     var url = "/data?size=" + size;
 
-    xmlhttp.onreadystatechange = function () {
+    xmlhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
             var data = JSON.parse(this.responseText);
             showData(data);
